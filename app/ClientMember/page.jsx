10ec8e19ea4902a651ccf9352ee1dffcae03b5fc
NickooMar <nicoo.marsili@gmail.com>
2023-11-10 20:08:57 -0300

    Providers

---
+++ app/ClientMember/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Member = () => {
+  return (
+    <div>
+      <h1>Member Client Session</h1>
+    </div>
+  );
+};
+
+export default Member;
---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,40 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log({ GithubProfile: profile });
+
+        let userRole = "GitHub User";
+        if (profile?.email === "nicoo.marsili@gmail.com") {
+          userRole = "admin";
+        }
+
+        return { ...profile, role: userRole };
+      },
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      profile(profile) {
+        console.log({ GoogleProfile: profile });
+
+        return { ...profile, id: profile.sub, role: userRole };
+      },
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.role) session.user.role = token.role;
+      return session;
+    },
+  },
+};
